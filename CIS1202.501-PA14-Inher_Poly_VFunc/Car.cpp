2b75663e605a295db6dfcbe1f25080afd538121b
nick <nick@nick.nick>
2020-11-27 17:10:32 -0500

    Completed functions for Car.cpp

--- CIS1202.501-PA14-Inher_Poly_VFunc/Car.cpp
+++ CIS1202.501-PA14-Inher_Poly_VFunc/Car.cpp
@@ -2,20 +2,22 @@
 
 Car::Car()
 {
-
+	numDoors = 2;
 }
 
-void Car::setNumDoors(int)
+void Car::setNumDoors(int num)
 {
-
+	numDoors = num;
 }
 
 int Car::getNumDoors()
 {
-
+	return numDoors;
 }
 
 void Car::displayInfo()
 {
+	Vehicle::displayInfo();
 
+	cout << "Doors: " << numDoors << endl;
 }
